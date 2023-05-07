abf2ef780cfeb18167e8e830803e1c130f1f86fb
AnwarBafadhl <442202992@student.ksu.edu.sa>
2023-05-08 02:41:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnlogin;
+    DB db;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btnlogin = findViewById(R.id.btnsignin);
+
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("") || pass.equals("")) {
+                    Toast.makeText(MainActivity.this, "All the fields are Require", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Boolean checkuserPass = db.checkUsernamePassword(user, pass);
+
+                    if(checkuserPass == true) {
+                        Toast.makeText(MainActivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivities(intent);
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Username or Password incorrect", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
